7aec08926cc8364478f65825b02a11402b9505c7
Kenny Szeto <kenny.szeto1@gmail.com>
2017-05-10 17:42:05 -0700

    None

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,83 +1,63 @@
 # IPND Stage 2 Final Project
 
-# You've built a Mad-Libs game with some help from Sean.
-# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
+import sys
 
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
+easy_quiz = "In Python, use the __1__ sign to assign values to variables. Python syntax is simple. For example, type in __2__ 'Hello, world!' to print the string. Functions start with keyword __3__ and take in any __4__ parameters."
+easy_answers = ["equal", "print", "def", "input"]
 
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
+medium_quiz = "The five types of variables are __1__, string, lists, tuples, and dictionary. Strings are defined as characters represented in __2__ marks. __3__ store many items separated by commas and enclosed within square brackets. The values stored in lists can be changed meaning they are __4__. Tuple is similar to lists but a key difference is that the values enclosed are read-only. This means tuples are __5__."
+medium_answers = [ "numbers", "quotation","lists", "mutable", "immutable"]
 
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
+hard_quiz = "In order to create a function, it must be __1__ before it is used. The function __2__ starts with keyword def, followed by a given __3__, and then __4__. Functions can use any number of __5__ that respond to the __6__ in the parentheses."
+hard_answers = ["defined", "block", "name", "parentheses", "arguments", "parameters"]
 
-# sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-# adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-# don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-# tuple, and ___4___ or can be more complicated such as objects and lambda functions.'
-
+#User is asked for difficulty level
 print "Please type a game difficulty to get started!"
-level = raw_input("Possible choices include easy, medium, and hard.") #User is asked for difficulty level
+level = raw_input("Possible choices include easy, medium, and hard.")
 
-missing_blanks = ["Aang", "the last airbender"]
+# After user selects difficulty level, play_game runs through the quiz and answer.
+# The game gives them 5 attempts to complete the quiz before it's game over.
+# User is prompted for a response to fill in the missing blanks. If they're incorrect,
+# they lose an attempt and are prompted to try again.
 
-test_string1 = "___1___ is a young air nomad and is ___2___!"
+def play_game (test_string, missing_values):
+    attempts_allowed = 5
+    for word in test_string.split():
+        if word.startswith("__"):
+            while attempts_allowed > 0:
+                user_input = raw_input("fill in the blank for " + word)
+                if user_input == missing_values[0]:
+                    test_string = test_string.replace(word, user_input)
+                    missing_values.remove(user_input)
+                    print "\nCorrect!" "\n" + test_string
+                    break
+                else:
+                    attempts_allowed = attempts_allowed - 1
+                    print 'You have '+ str(attempts_allowed) + ' attempts remaining.'
+            if attempts_allowed == 0:
+                print "You ran out of attempts! Game over!"
+                sys.exit()
 
-guesses = 5
+    print "\nYou won!"
 
-def word_in_blanks (word, missing_blanks):
-    for blanks in missing_blanks:
-        if blanks in word:
-            return blanks
-    return None
-
-def play_game (word_string, missing_blanks):
-    blankReplaced = []
-    word_string = word_string.split()
-    index = 0
-    for word in word_string:
-        user_input = raw_input("What should be substituted in for __" + str(index + 1) + "__?") #NEED TO FIX THIS. SEARCH FOR __#__ THROUGH POS AND ASK USER FOR REPLACEMENT
-        replacement = word_in_blanks(word, missing_blanks)
-        if user_input in missing_blanks:
-            word = word.replace("__" + str(index + 1) + "__", missing_blanks[index])
-            blankReplaced.append(word)
-            index = index + 1
-        else:
-            blankReplaced.append(word_string)
-    blankReplaced = " ".join(blankReplaced)
-    return blankReplaced
-
+#One of the following are executed depending on the user's choice of difficulty
 if level == 'easy':
     print "You've selected " + level + "!"
     print ('\n' "You get 5 guesses for each problem.")
 
-    print "The current paragraph reads as follows:" + '\n' + test_string1 #sample
-    print play_game(test_string1, missing_blanks)
+    print "The current paragraph reads as follows:" + '\n' + easy_quiz
+    play_game(easy_quiz, easy_answers)
 
+if level == 'medium':
+    print "You've selected " + level + "!"
+    print ('\n' "You get 5 guesses for each problem.")
 
-# if level == 'medium':
-#   print "You've selected " + level + "!!"
-# if level == 'hard':
-#   print "You've selected " + level + "!!!"
+    print "The current paragraph reads as follows:" + '\n' + medium_quiz
+    play_game(medium_quiz, medium_answers)
 
+if level == 'hard':
+    print "You've selected " + level + "!"
+    print ('\n' "You get 5 guesses for each problem.")
 
-
-
-# The answer for ___1___ is 'function'. Can you figure out the others?
-
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
-
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
-
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
+    print "The current paragraph reads as follows:" + '\n' + hard_quiz
+    play_game(hard_quiz, hard_answers)
--- playground.py
+++ playground.py
@@ -3,20 +3,31 @@ import sys
 easy_quiz = "In Python, use the __1__ sign to assign values to variables. Python syntax is simple. For example, type in __2__ 'Hello, world!' to print the string. Functions start with keyword __3__ and take in any __4__ parameters."
 easy_answers = ["equal", "print", "def", "input"]
 
+medium_quiz = "The five types of variables are __1__, string, lists, tuples, and dictionary. Strings are defined as characters represented in __2__ marks. __3__ store many items separated by commas and enclosed within square brackets. The values stored in lists can be changed meaning they are __4__. Tuple is similar to lists but a key difference is that the values enclosed are read-only. This means tuples are __5__."
+medium_answers = [ "numbers", "quotation","lists", "mutable", "immutable"]
+
+hard_quiz = "In order to create a function, it must be __1__ before it is used. The function __2__ starts with keyword def, followed by a given __3__, and then __4__. Functions can use any number of __5__ that respond to the __6__ in the parentheses."
+hard_answers = ["defined", "block", "name", "parentheses", "arguments", "parameters"]
+
+#User is asked for difficulty level
 print "Please type a game difficulty to get started!"
-level = raw_input("Possible choices include easy, medium, and hard.") #User is asked for difficulty level
+level = raw_input("Possible choices include easy, medium, and hard.")
+
+# After user selects difficulty level, play_game runs through the quiz and answer.
+# The game gives them 5 attempts to complete the quiz before it's game over.
+# User is prompted for a response to fill in the missing blanks. If they're incorrect,
+# they lose an attempt and are prompted to try again.
 
 def play_game (test_string, missing_values):
-    output = ''
     attempts_allowed = 5
     for word in test_string.split():
         if word.startswith("__"):
             while attempts_allowed > 0:
                 user_input = raw_input("fill in the blank for " + word)
                 if user_input == missing_values[0]:
-                    output += ' ' + user_input
+                    test_string = test_string.replace(word, user_input)
                     missing_values.remove(user_input)
-                    print "\nCorrect!"
+                    print "\nCorrect!" "\n" + test_string
                     break
                 else:
                     attempts_allowed = attempts_allowed - 1
@@ -24,14 +35,27 @@ def play_game (test_string, missing_values):
             if attempts_allowed == 0:
                 print "You ran out of attempts! Game over!"
                 sys.exit()
-        else:
-            output += ' ' + word
 
-    print output.lstrip() + "\nYou won!"
+    print "\nYou won!"
 
+#One of the following are executed depending on the user's choice of difficulty
 if level == 'easy':
     print "You've selected " + level + "!"
     print ('\n' "You get 5 guesses for each problem.")
 
     print "The current paragraph reads as follows:" + '\n' + easy_quiz
-    print play_game(easy_quiz, easy_answers)
+    play_game(easy_quiz, easy_answers)
+
+if level == 'medium':
+    print "You've selected " + level + "!"
+    print ('\n' "You get 5 guesses for each problem.")
+
+    print "The current paragraph reads as follows:" + '\n' + medium_quiz
+    play_game(medium_quiz, medium_answers)
+
+if level == 'hard':
+    print "You've selected " + level + "!"
+    print ('\n' "You get 5 guesses for each problem.")
+
+    print "The current paragraph reads as follows:" + '\n' + hard_quiz
+    play_game(hard_quiz, hard_answers)
