2d9dbffd4c48ff32fbee565d0e9c5efc17ffad11
Kenny Szeto <kenny.szeto1@gmail.com>
2017-05-12 17:01:14 -0700

    Defined difficulty_level function

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -11,16 +11,13 @@ medium_answers = [ "numbers", "quotation","lists", "mutable", "immutable"]
 hard_quiz = "In order to create a function, it must be __1__ before it is used. The function __2__ starts with keyword def, followed by a given __3__, and then __4__. Functions can use any number of __5__ that respond to the __6__ in the parentheses."
 hard_answers = ["defined", "block", "name", "parentheses", "arguments", "parameters"]
 
-#User is asked for difficulty level
-print "Please type a game difficulty to get started!"
-level = raw_input("Possible choices include easy, medium, and hard.")
 
 # After user selects difficulty level, play_game runs through the quiz and answer.
 # The game gives them 5 attempts to complete the quiz before it's game over.
 # User is prompted for a response to fill in the missing blanks. If they're incorrect,
 # they lose an attempt and are prompted to try again.
 
-def play_game (test_string, missing_values):
+def play_game(test_string, missing_values):
     attempts_allowed = 5
     for word in test_string.split():
         if word.startswith("__"):
@@ -33,31 +30,41 @@ def play_game (test_string, missing_values):
                     break
                 else:
                     attempts_allowed = attempts_allowed - 1
-                    print 'You have '+ str(attempts_allowed) + ' attempts remaining.'
+                    print 'You have ' + str(attempts_allowed) + ' attempts remaining.'
             if attempts_allowed == 0:
                 print "You ran out of attempts! Game over!"
                 sys.exit()
 
     print "\nYou won!"
 
-#One of the following are executed depending on the user's choice of difficulty
-if level == 'easy':
-    print "You've selected " + level + "!"
-    print ('\n' "You get 5 guesses for each problem.")
+# One of the following are executed depending on the user's choice of difficulty
+def difficulty_level(level):
+    if level == 'easy':
+        print "You've selected " + level + "!"
+        print ('\n' "You get 5 guesses for each problem.")
 
-    print "The current paragraph reads as follows:" + '\n' + easy_quiz
-    play_game(easy_quiz, easy_answers)
+        print "The current paragraph reads as follows:" + '\n' + easy_quiz
+        play_game(easy_quiz, easy_answers)
 
-if level == 'medium':
-    print "You've selected " + level + "!"
-    print ('\n' "You get 5 guesses for each problem.")
+    if level == 'medium':
+        print "You've selected " + level + "!"
+        print ('\n' "You get 5 guesses for each problem.")
 
-    print "The current paragraph reads as follows:" + '\n' + medium_quiz
-    play_game(medium_quiz, medium_answers)
+        print "The current paragraph reads as follows:" + '\n' + medium_quiz
+        play_game(medium_quiz, medium_answers)
 
-if level == 'hard':
-    print "You've selected " + level + "!"
-    print ('\n' "You get 5 guesses for each problem.")
+    if level == 'hard':
+        print "You've selected " + level + "!"
+        print ('\n' "You get 5 guesses for each problem.")
 
-    print "The current paragraph reads as follows:" + '\n' + hard_quiz
-    play_game(hard_quiz, hard_answers)
+        print "The current paragraph reads as follows:" + '\n' + hard_quiz
+        play_game(hard_quiz, hard_answers)
+
+#User is asked for difficulty level and then call on difficulty_level.
+print "Please type a game difficulty to get started!"
+level = raw_input("Possible choices include easy, medium, and hard.")
+difficulty_level(level)
+
+
+
+
